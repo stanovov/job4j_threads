1c17ae3a12250f3bcff0fd752593c3a0ebf3dcd3
Semyon Stanovov <sestanovov@gmail.com>
2021-08-30 16:24:53 +0300

    3.1. Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -5,15 +5,17 @@ public class ConsoleProgress implements Runnable {
     public void run() {
         String[] process = {"-", "\\", "|", "/"};
         int index = 0;
-        try {
-            while (!Thread.currentThread().isInterrupted()) {
-                System.out.print("\r load: " + process[index++]);
-                if (index == process.length) {
-                    index = 0;
-                }
-                Thread.sleep(500);
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r load: " + process[index++]);
+            if (index == process.length) {
+                index = 0;
             }
-        } catch (InterruptedException ignored) { }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ignored) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
