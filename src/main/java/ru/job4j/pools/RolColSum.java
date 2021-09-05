563755495f77a1c5aa38c2af4eaef60a6d10d5d4
Semyon Stanovov <sestanovov@gmail.com>
2021-09-05 21:10:10 +0300

    4. CompletableFuture [#361626 #198610] - fix

--- src/main/java/ru/job4j/pools/RolColSum.java
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -1,6 +1,5 @@
 package ru.job4j.pools;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -47,21 +46,19 @@ public class RolColSum {
     }
 
     public static Sums[] sum(int[][] matrix) {
-        validateMatrix(matrix);
         int size = Math.max(matrix.length, matrix[0].length);
         final Sums[] sums = new Sums[size];
         for (int i = 0; i < sums.length; i++) {
-            sums[i] = getSums(matrix, i);
+            sums[i] = getSums(matrix, i, size);
         }
         return sums;
     }
 
     public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
-        validateMatrix(matrix);
         final int size = Math.max(matrix.length, matrix[0].length);
         Map<Integer, CompletableFuture<Sums>> futures = new HashMap<>();
         for (int i = 0; i < size; i++) {
-            futures.put(i, getTask(matrix, i));
+            futures.put(i, getTask(matrix, i, size));
         }
         final Sums[] sums = new Sums[size];
         for (Integer key : futures.keySet()) {
@@ -70,38 +67,21 @@ public class RolColSum {
         return sums;
     }
 
-    private static void validateMatrix(int[][] matrix) {
-        if (matrix.length == 0) {
-            throw new IllegalArgumentException();
-        }
-        if (matrix[0].length == 0) {
-            throw new IllegalArgumentException();
-        }
-        int len = matrix[0].length;
-        for (int i = 1; i < matrix.length; i++) {
-            if (matrix[i].length != len) {
-                throw new IllegalArgumentException();
-            }
-        }
-    }
-
-    private static Sums getSums(int[][] matrix, int i) {
+    private static Sums getSums(int[][] matrix, int i, int size) {
         int rowSum = 0;
-        if (i < matrix.length) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                rowSum += matrix[i][j];
-            }
-        }
         int colSum = 0;
-        for (int j = 0; j < matrix.length; j++) {
-            if (i < matrix[j].length) {
+        for (int j = 0; j < size; j++) {
+            if (j < matrix.length && i < matrix[j].length) {
                 colSum += matrix[j][i];
+            }
+            if (i < matrix.length && j < matrix[i].length) {
+                rowSum += matrix[i][j];
             }
         }
         return new Sums(rowSum, colSum);
     }
 
-    private static CompletableFuture<Sums> getTask(int[][] matrix, int i) {
-        return CompletableFuture.supplyAsync(() -> getSums(matrix, i));
+    private static CompletableFuture<Sums> getTask(int[][] matrix, int i, int size) {
+        return CompletableFuture.supplyAsync(() -> getSums(matrix, i, size));
     }
 }
