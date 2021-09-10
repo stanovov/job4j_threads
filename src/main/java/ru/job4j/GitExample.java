d6be72969e09bb6c96d40c536f033009c3c10b18
Semyon Stanovov <sestanovov@gmail.com>
2021-09-10 13:37:28 +0300

    3.2. Откат изменений. [#307144]

---
+++ src/main/java/ru/job4j/GitExample.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class GitExample {
+    public static void main(String[] args) {
+        System.out.println("First");
+        System.out.println("Second");
+        System.out.println("Third");
+    }
+}
